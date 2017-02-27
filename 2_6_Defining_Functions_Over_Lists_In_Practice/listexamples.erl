97b5f3c186685e3abbcc91233b6b0d4b542f7daf
Simon Hellinger <simon.hellinger@cgm.com>
2017-02-27 16:08:32 +0100

    Added 'max' recursion to the example

--- 2_6_Defining_Functions_Over_Lists_In_Practice/listexamples.erl
+++ 2_6_Defining_Functions_Over_Lists_In_Practice/listexamples.erl
@@ -1,7 +1,7 @@
 -module(listexamples).
 -include_lib("eunit/include/eunit.hrl").
 
--export([productTail/1, productDirect/1]).
+-export([productTail/1, productDirect/1, maxTail/1, maxDirect/1]).
 
 % tail recursion of product
 productTail(Xs) ->
@@ -18,7 +18,23 @@ productDirect([]) ->
 productDirect([H|T]) ->
     H * productDirect(T).
 
-%% product tests
+% tail recursion of max
+maxTail([X|Xs]) ->
+    maxTail(Xs, X).
+
+maxTail([], Acc) -> 
+    Acc;
+maxTail([H|T], Acc) ->
+    maxTail(T, max(Acc, H)).
+
+% direct recursion of max
+maxDirect([H]) ->
+    H;
+maxDirect([H|T]) ->
+    max(H, maxDirect(T)).
+
+%% Tests
+% product tests
 productTail_test() ->
     ?assertEqual(0, productTail([1, 2, 0, 4])),
     ?assertEqual(1, productTail([])),
@@ -31,9 +47,13 @@ productDirect_test() ->
     ?assertEqual(1, productDirect([1])),
     ?assertEqual(120, productDirect([1,2,3,5,4])).
 
-%max(Xs) -> Xs.
-%
-%max_test() ->
-%    ?assertError(function_call, productTail([])),
-%    ?assertEqual(1, productTail([1])),
-%    ?assertEqual(5, productTail([1,2,3,5,4])).
+% max tests
+maxTail_test() ->
+    ?assertError(function_clause, maxTail([])),
+    ?assertEqual(1, maxTail([1])),
+    ?assertEqual(5, maxTail([1,2,3,5,4])).
+
+maxDirect_test() ->
+    ?assertError(function_clause, maxDirect([])),
+    ?assertEqual(1, maxDirect([1])),
+    ?assertEqual(5, maxDirect([1,2,3,5,4])).

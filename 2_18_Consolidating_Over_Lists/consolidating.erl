91e73d9bc6389cd42c8a648dadb9497bc6168873
Simon Hellinger <simon.hellinger@cgm.com>
2017-03-03 13:47:08 +0100

    Added 'quicksort' to the functions, restructured a little

--- 2_18_Consolidating_Over_Lists/consolidating.erl
+++ 2_18_Consolidating_Over_Lists/consolidating.erl
@@ -1,9 +1,11 @@
 -module(consolidating).
 -include_lib("eunit/include/eunit.hrl").
 
--export([join/2, concat/1]).
+-export([join/2, concat/1, merge_sort/1, quick_sort/1, insertion_sort/1]).
 
+%%
 %% joins two lists (and, thus, strings) together.
+%%
 join(Xs, []) ->
     Xs;
 join(Xs, Ys) ->
@@ -32,7 +34,9 @@ join_test() ->
     ?assertEqual("ab", join("a", "b")),
     ?assertEqual("Hello World", join("Hello", " World")).
 
+%%
 %% Concatenation of "unlimited"" lists into one list.
+%%
 concat(Xs) ->
     concat(Xs, []).
 
@@ -46,7 +50,9 @@ concat_test() ->
     ?assertEqual([1, 2, 3, 4], concat([[1], [2, 3], [4]])),
     ?assertEqual("goodbye, world", concat(["goo", "dbye,", " ", "world"])).
 
+%%
 %% Checks whether an element is in a list
+%%
 member(_, []) ->
     false;
 member(N, [X|Xs]) ->
@@ -64,28 +70,9 @@ member_test() ->
     ?assertEqual(true, member(2, [1, 3, 4, 2, 6])),
     ?assertEqual(true, member([3, 4], [1, 2, [3, 4], 5])).
 
-%% Merge sort
-%% The actual sorting happens in merge.
-merge_sort([]) ->
-    [];
-merge_sort([N]) ->
-    [N];
-merge_sort(L) ->
-    [Left, Right] = split(L),
-    merge(merge_sort(Left), merge_sort(Right)).
-
-% merges a list piece by piece, ordering it on the way
-merge(A, []) ->
-    A;
-merge([], B) ->
-    B;
-merge([A|As] = LA, [B|Bs] = LB) ->
-    case A < B of
-        true ->
-            [A | merge(As, LB)];
-        false ->
-            [B | merge(LA, Bs)]
-    end.
+%%
+%% Sorting algorightm helpers
+%%
 
 % Returns the length of a list
 len(L) ->
@@ -107,13 +94,66 @@ split(L, R, 0) ->
 split([L|Ls], Right, Elems) ->
     split(Ls, [L|Right], Elems - 1).
 
-merge_sort_test() ->
-    ?assertEqual([], merge_sort([])),
-    ?assertEqual([1], merge_sort([1])),
-    ?assertEqual([1, 2], merge_sort([2, 1])),
-    ?assertEqual([1, 2, 3, 4, 5, 6, 7, 70, 900], merge_sort([900, 1, 70, 4, 2, 3, 5, 6, 7])).
+%%
+%% Merge sort
+%%
 
+%% The actual sorting happens in merge.
+merge_sort([]) ->
+    [];
+merge_sort([N]) ->
+    [N];
+merge_sort(L) ->
+    [Left, Right] = split(L),
+    merge(merge_sort(Left), merge_sort(Right)).
+
+% This is the workhorse of merge_sort
+merge(A, []) ->
+    A;
+merge([], B) ->
+    B;
+merge([A|As] = LA, [B|Bs] = LB) ->
+    case A < B of
+        true ->
+            [A | merge(As, LB)];
+        false ->
+            [B | merge(LA, Bs)]
+    end.
+
+%% Quick sort
+quick_sort([]) ->
+    [];
+quick_sort([X|_Xs] = L) ->
+    {Left, Right} = partition(L, X),
+    concat([quick_sort(Left), [X], quick_sort(Right)]).
+
+% this is the workhorse of quick_sort
+partition(L, P) ->
+    partition(L, P, [], []).
+
+partition([], _, Left, Right) ->
+    {Left, Right};
+partition([X|Xs], Pivot, Left, Right) when Pivot == X ->
+    partition(Xs, Pivot, Left, Right);
+partition([X|Xs], Pivot, Left, Right) ->
+    case Pivot > X of
+        true ->
+            partition(Xs, Pivot, [X|Left], Right);
+        false ->
+            partition(Xs, Pivot, Left, [X|Right])
+    end.
+
+
+partition_test() ->
+    ?assertEqual({[], []}, partition([], 0)),
+    ?assertEqual({[], [1]}, partition([1], 0)),
+    ?assertEqual({[], [2]}, partition([2, 1], 1)),
+    ?assertEqual({[5, 2, 3],  [8, 9, 7]}, partition([3, 2, 5, 6, 7, 9, 8], 6)).
+
+
+%%
 %% Insertion sort
+%%
 insertion_sort([]) ->
     [];
 insertion_sort([X|Xs]) ->
@@ -129,8 +169,33 @@ insert(X, [Y|Ys] = A) ->
             [Y | insert(X, Ys)]
     end.
 
-insertion_sort_test() ->
-    ?assertEqual([], insertion_sort([])),
-    ?assertEqual([1], insertion_sort([1])),
-    ?assertEqual([1, 2], insertion_sort([2, 1])),
-    ?assertEqual([1, 2, 3, 4, 5, 6, 7, 70, 900], insertion_sort([900, 1, 70, 4, 2, 3, 5, 6, 7])).
+%%
+%% Sorting tests
+%%
+empty_list_test() ->
+    Act = [],
+    Exp = [],
+    ?assertEqual(Exp, merge_sort(Act)),
+    ?assertEqual(Exp, quick_sort(Act)),
+    ?assertEqual(Exp, insertion_sort(Act)).
+
+single_element_list_test() ->
+    Act = [1],
+    Exp = [1],
+    ?assertEqual(Exp, merge_sort(Act)),
+    ?assertEqual(Exp, quick_sort(Act)),
+    ?assertEqual(Exp, insertion_sort(Act)).
+
+two_element_list_test() ->
+    Act = [2, 1],
+    Exp = [1, 2],
+    ?assertEqual(Exp, merge_sort(Act)),
+    ?assertEqual(Exp, quick_sort(Act)),
+    ?assertEqual(Exp, insertion_sort(Act)).
+
+regular_list_test() ->
+    Act = [900, 1, 70, 4, 2, 3, 5, 6, 7],
+    Exp = [1, 2, 3, 4, 5, 6, 7, 70, 900],
+    ?assertEqual(Exp, merge_sort(Act)),
+    ?assertEqual(Exp, quick_sort(Act)),
+    ?assertEqual(Exp, insertion_sort(Act)).

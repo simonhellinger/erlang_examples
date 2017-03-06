2fdb6c5602366b036faa1ed90fed3928f1340d29
Simon Hellinger <simon.hellinger@cgm.com>
2017-03-06 09:29:38 +0100

    Added higher order functions 'evens' and 'product'

--- 3_5_Higher-order_functions_in_practice/hofs.erl
+++ 3_5_Higher-order_functions_in_practice/hofs.erl
@@ -1,13 +1,30 @@
 -module(hofs).
 -include_lib("eunit/include/eunit.hrl").
 
--export([double_all/1]).
+-export([double_all/1, evens/1]).
 
 
 double_all(Xs) -> lists:map(fun (X) -> X * 2 end, Xs).
 
+evens(Xs) -> lists:filter(fun (X) -> X rem 2 == 0 end, Xs).
+
+product(Xs) -> lists:foldr(fun(X, Prod) -> Prod * X end, 1, Xs).
 
 double_all_test()->
     ?assertEqual([], double_all([])),
     ?assertEqual([0], double_all([0])),
     ?assertEqual([2, 4, 6], double_all([1, 2, 3])).
+
+evens_test() ->
+    ?assertEqual([],evens([])),
+    ?assertEqual([],evens([1, 3, 5])),
+    ?assertEqual([0, 2, 4],evens([0, 1, 2, 3, 4])).
+
+product_test() ->
+    ?assertEqual(1 ,product([])),
+    ?assertEqual(0 ,product([0, 1, 2])),
+    ?assertEqual(24 ,product([1, 2, 3, 4])).
+
+
+
+% TODO: try property-based testing

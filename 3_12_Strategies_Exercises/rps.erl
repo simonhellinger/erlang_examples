ae860ac135c0abcf4d5fc0b483506d6d888da4e9
Simon Hellinger <simon.hellinger@cgm.com>
2017-03-09 10:39:05 +0100

    Added strategies to rock-paper-scissors

--- 3_12_Strategies_Exercises/rps.erl
+++ 3_12_Strategies_Exercises/rps.erl
@@ -1,5 +1,6 @@
 -module(rps).
--export([play/1,echo/1,play_two/3,rock/1,no_repeat/1,const/1,enum/1,cycle/1,rand/1,val/1,tournament/2]).
+-include_lib("eunit/include/eunit.hrl").
+-export([play/1,echo/1,play_two/3,rock/1,no_repeat/1,const/1,enum/1,cycle/1,rand/1,beat_least_frequent/1,beat_most_frequent/1,random_strategy/1,val/1,tournament/2]).
 
 
 %
@@ -104,6 +105,13 @@ beats(paper) ->
 beats(scissors) ->
     paper.
 
+loses(rock) ->
+    paper;
+loses(paper) ->
+    scissors;
+loses(scissors) ->
+    rock.
+
 %
 % strategies.
 %
@@ -119,17 +127,88 @@ rock(_) ->
 
 % FOR YOU TO DEFINE
 % REPLACE THE dummy DEFINITIONS
+% Function parameter is always the opponents list of plays
 
+% Play what would've beaten by opponent's latest play, won't beat it now
+% as opponent won't repeat.
 no_repeat([]) ->
-    dummy;
-no_repeat([X|_]) ->
-    dummy.
+    paper;
+no_repeat([X|_]) -> % opponent does not repeat
+    beats(X).       % take the one she would've beat now, so she won't beat it next time
 
 const(Play) ->
     dummy.
 
 cycle(Xs) ->
-    dummy.
+    enum(length(Xs) rem 3).
+
+% Choose next play randomly.
 
 rand(_) ->
-    dummy.
+    enum(rand:uniform(3) - 1).
+
+% Uses the element played most by the opponent, assuming
+% it will play it more than the others.
+beat_most_frequent(Xs) -> 
+    {R, P, S} = bin(Xs),
+    MstFrq = max(max(R, P), S),
+    OpponentsPlay = 
+        case MstFrq of
+            R -> rock;
+            P -> paper;
+            S -> scissors
+        end,
+    loses(OpponentsPlay).
+
+% Uses the element played least by the opponent, assuming the opponent
+% will play all elements equally in the long run.
+beat_least_frequent(Xs) -> 
+    {R, P, S} = bin(Xs),
+    LstFrq = min(min(R, P), S),
+    OpponentsPlay = 
+        case LstFrq of
+            R -> rock;
+            P -> paper;
+            S -> scissors
+        end,
+    loses(OpponentsPlay).
+
+% Selects each played hand into one of the bins {R, P, S}.
+bin(Xs) ->
+    lists:foldr(fun bin/2, {0, 0, 0}, Xs).
+
+bin(rock, {R, P, S}) ->
+    {R + 1, P, S};
+bin(paper, {R, P, S}) ->
+    {R, P + 1, S};
+bin(scissors, {R, P, S}) ->
+    {R, P, S + 1}.
+
+
+random_strategy(Strategies) ->
+    N = rand:uniform(length(Strategies)),
+    lists:nth(N, Strategies).
+
+% Strategy tests
+
+cycle_test() -> % cycle depends on the length of the list
+    ?assertEqual(rock, cycle([])),
+    ?assertEqual(paper, cycle([rock, rock, paper, scissors])),
+    ?assertEqual(scissors, cycle([rock, rock, paper, scissors, rock])),
+    ?assertEqual(rock, cycle([rock, rock, paper, scissors, rock, scissors])).
+
+no_repeat_test() ->
+    ?assertEqual(paper, no_repeat([])),
+    ?assertEqual(rock, no_repeat([paper])).
+
+beat_most_frequent_test() ->
+    ?assertEqual(paper, beat_most_frequent([])),
+    ?assertEqual(paper, beat_most_frequent([paper, rock, paper, rock, scissors, rock])),
+    ?assertEqual(scissors, beat_most_frequent([paper, rock, paper, rock, paper, paper])),
+    ?assertEqual(rock, beat_most_frequent([scissors, rock, scissors, rock, scissors, scissors])).
+
+beat_least_frequent_test() ->
+    ?assertEqual(paper, beat_least_frequent([])),
+    ?assertEqual(rock, beat_least_frequent([paper, rock, paper, rock, scissors, rock])),
+    ?assertEqual(rock, beat_least_frequent([paper, rock, paper, rock, paper, paper])),
+    ?assertEqual(scissors, beat_least_frequent([scissors, rock, scissors, rock, scissors, scissors])).
